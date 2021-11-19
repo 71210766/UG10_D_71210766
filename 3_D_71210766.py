4d8e0c9de34f81b79240973953f6e72e458725bb
71210766 <93719122+71210766@users.noreply.github.com>
2021-11-19 11:53:46 +0800

    Add files via upload

---
+++ 3_D_71210766.py
@@ -0,0 +1,7 @@
+Daftar1 = input("Masukkan daftar belanja Anda : ")
+List1 = Daftar1.title(sAbuN mandi, sHampo, sIkat Gigi)
+List2 = List1.split(['Sabun Mandi', 'Shampo', 'Sikat Gigi'])
+print("Daftar belanja : ", List2)
+if (Daftar2.title()in List2) == True :
+    print("Barang",Daftar2.upper(sUsu coklaT),"Sudah berada dalam daftar belanja")
+else :
